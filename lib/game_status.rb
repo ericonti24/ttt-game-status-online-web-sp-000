6123343283917c455fe0c28ca0f861fd7c803975
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-20 14:57:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
    position_1 == "X" && position_2 == "X" && position_3 == "X"
    return win_combination
  end
- if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
    return false
  end
 end
