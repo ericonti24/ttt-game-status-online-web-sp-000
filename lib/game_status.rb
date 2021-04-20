e3a80ed8b8e2b085f4b0eb95f2c69cce0cedd031
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-20 14:39:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
    position_1 = board[win_index_1] # value of board at win_index_1
    position_2 = board[win_index_2] # value of board at win_index_2
    position_3 = board[win_index_3] # value of board at win_index_3
-   
+
    position_1 == "X" position_2 == "X" && position_3 == "X"
    return WIN_COMBINATIONS
  end
